b9f7408cc1827ed85dc11183834b27935e7bc5d3
A-007481D <labidabdelmalek@gmail.com>
2025-12-15 15:00:10 +0100

    CHORE: init sprinboot project, docker setup,  & CI pipelines. CLUS-1 #done

---
+++ src/main/java/com/progressoft/assignment/clustered_data_warehouse/ClusteredDataWarehouseApplication.java
@@ -0,0 +1,14 @@
+package com.progressoft.assignment.clustered_data_warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClusteredDataWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClusteredDataWarehouseApplication.class, args);
+	}
+
+}
+
