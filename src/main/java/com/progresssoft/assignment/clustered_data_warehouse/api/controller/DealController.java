4b8d1451c676a728b1ccd0adae5d43cedcb95b89
A-007481D <labidabdelmalek@gmail.com>
2025-12-16 16:27:21 +0100

    fix: enable correct list validation
    - Add @Validated and @Valid annotations to enforce validation on list items in DealController

--- src/main/java/com/progresssoft/assignment/clustered_data_warehouse/api/controller/DealController.java
+++ src/main/java/com/progresssoft/assignment/clustered_data_warehouse/api/controller/DealController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +21,13 @@ import java.util.List;
 @RequestMapping("/api/v1/deals")
 @RequiredArgsConstructor
 @Slf4j
+@Validated
 public class DealController {
 
     private final DealService dealService;
 
     @PostMapping
-    public ResponseEntity<String> importDeals(@RequestBody @Valid List<DealRequestDTO> dealRequests) {
+    public ResponseEntity<String> importDeals(@RequestBody List< @Valid DealRequestDTO> dealRequests) {
 
         log.info("Recieved request to import {} deals ", dealRequests.size());
         ImportSummaryDTO summary = dealService.importDeals(dealRequests);
